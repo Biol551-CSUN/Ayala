3b9d87600f184528a73c9e60ef8b91fdca47665d
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-07 14:38:44 -0800

    Always pull before you push :)

---
+++ Week_03/Script/Script2023-07-02.R
@@ -0,0 +1,10 @@
+### This is my second script and I am learning how to ggplot
+### Created by: Nicole Ayala
+### Created on: 2023-02-07
+#####################################################################
+### Load libraries #######
+library(tidyverse)
+library(palmerpenguins)
+### use glimpse ######
+glimpse(penguins)
+
