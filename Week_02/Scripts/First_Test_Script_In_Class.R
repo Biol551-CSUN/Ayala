1bc66264797f45ec134d5d9284a60a44c3e90d5c
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-14 14:47:19 -0800

    This was my first intor to creating a legit script and how to import data and  installing packages to create libraries, etc.

---
+++ Week_02/Scripts/First_Test_Script_In_Class.R
@@ -0,0 +1,14 @@
+### This is my first script and I am learning how to import data
+### Created by: Nicole Ayala
+### Created on: 2023-02-02
+#####################################################################
+### Load libraries #######
+library(tidyverse)
+library(here)
+### Read in data ###
+WeightData<-read_csv(here("Week_02","Data","weightdata.csv"))
+### Data Analysis #######
+head(WeightData) # Looks at the top 6 lines of the dataframe
+tail(WeightData) # Looks at the bottom 6 lines of the dataframe
+View(WeightData) # opens a new window to look at the entire dataframe
+glimpse(WeightData) # tells you if it is a character or  numeric
