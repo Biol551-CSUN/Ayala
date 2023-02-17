0cb6fb615e3b4bb49107f156544badeb75e1550e
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-16 16:32:22 -0800

    we have finally come out of the box plot phase, here is a density plot and the script to the density csv file :)

--- Week_04/Script/HWAssignment(2023-02-16).R
+++ Week_04/Script/HWAssignment(2023-02-16).R
@@ -1,34 +1,52 @@
-### today we will be going over tidyr 
+### put a Bernie on it LOL
 ### Created by: Nicole Ayala
 ### Created on: 2023-02-16
 #####################################################################
 ### Load libraries #######
 library(tidyverse)
 library(here)
+library(beyonce)
+library(ggridges)
 
-## Load Data #####
-Chemdata<-read_csv(here("Week_04","Data","chemicaldata_maunalua.csv"))
-View(Chemdata)
-glimpse(Chemdata)
+### Load Data #####
 Chemdata_clean<-Chemdata %>% # remember the PIPE!!!!
-      filter(complete.cases(.)) #filters out everything that is not a complete row
-View(Chemdata_clean)
-drop_na() %>% #filters out everything that is not a complete row
-  separate(col = Tide_time, # choose the tide time col, things that exist as data do not have quotes but what we are creating are in quotes
-           into = c("Tide","Time"), # separate it into two columns Tide and time
-           sep = "_" ) # separate by _
-remove = FALSE # keep the original tide_time column
-unite(col = "Site_Zone", # the name of the NEW col
-      c(Site,Zone), # the columns to unite
-      sep = ".", # lets put a . in the middle, put anything in the middle! space, question mark, bracket, comma 
-      remove = FALSE) # keep the original
+  filter(complete.cases(.)) #filters out everything that is not a complete row
 
-head(Chemdata_clean)
+View(Chemdata_clean)
+
+Chemdata_clean<-Chemdata %>%
+  drop_na() %>% #filters out everything that is not a complete r
+  separate(col = Tide_time, # choose the tide time col, things that exist as data do not have quotes
+                            # but what we are creating are in quotes
+           into = c("tide","time"), # separate it into two columns, named them tide and time ) # separate by a space
+           remove = FALSE) # keep the original tide_time column
 
 Chemdata_long<-Chemdata_clean %>%
-  pivot_longer(cols = Temp_in:percent_sgd, # the cols you want to pivot. This says select the temp to percent SGD cols
+  pivot_longer(cols = Temp_in:pH, # the cols you want to pivot. This says select the temp to pH cols
                names_to = "Variables", # the names of the new cols with all the column names
-               values_to = "Values") # names of the new column with all the values
+               values_to = "Values") %>%# names of the new column with all the values
+  group_by(Variables, Season, Zone ) %>% # group by everything we want 
+  summarise(Param_means = mean(Values, na.rm = TRUE), # get mean 
+            Param_vars = var(Values, na.rm = TRUE), # get variance
+            Param_stdv = sd(Values, na.rm = TRUE)) # getting the standard deviation
 
-View(Chemdata_long)
+
+write_csv(here("Week_04","Output","InClassDensityPlot(2023-02-16).csv")) # export as a csv to the right folder
+
+View(Chemdata_long) # this upper section is related to the csv file, plot info is down below!
+
+
+          
+chemplot<-Chemdata %>% # make sure to keep putting a pipeline & name this chemdata separate from chemdata_long!
+  drop_na()%>% # ALWAYS ADD THE DROP_NA TO REMOVE NAs IN YOUR PLOT
+  mutate(pH_log = log(pH))%>% # PARANTHESE! Be mindful of what you name or rename (pH)
+  ggplot(aes(x = pH_log, y = Zone, fill = Season))+ 
+  geom_density_ridges2() +
+  labs(x = "Season", y = "Zone", title = "Each Zone's Environmental Differences within Basicity and Acidity During the Spring Season")+
+            scale_fill_manual(values = beyonce_palette(2)) +
+  theme(axis.title = element_text(size = 14, color = "green"),
+        panel.background = element_rect(fill = "white"))
+        
   
+chemplot
+
