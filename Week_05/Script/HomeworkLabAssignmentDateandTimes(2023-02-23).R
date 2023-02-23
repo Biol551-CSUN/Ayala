dc9186d03982a8a044751a8063b6899dd30c88ad
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-23 15:32:07 -0800

    got the join to work :)

--- Week_05/Script/HomeworkLabAssignmentDateandTimes(2023-02-23).R
+++ Week_05/Script/HomeworkLabAssignmentDateandTimes(2023-02-23).R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(here)
 library(lubridate)
 library(devtools)
+library(ggridges)
 
 install_github("Gibbsdavidl/CatterPlots") # install the data
 
@@ -15,8 +16,7 @@ install_github("Gibbsdavidl/CatterPlots") # install the data
 # Depth data
 DepthData<-read_csv(here("Week_05","Data","DepthData.csv")) 
 DepthData<-DepthData %>%
-  mutate(Date = as.character(Date), # CHANGE DATE INTO A CHARACTER BUT HERE IT IS ALREADY A CHARACTER
-         Date = mdy_hm(Date)) # change date format to mdy_hm
+  mutate(date = mdy_hm(date)) # change date format to mdy_hm
 
 View(DepthData)
 glimpse(DepthData)
@@ -24,35 +24,21 @@ glimpse(DepthData)
 # Conductivity data from each site
 #mutate(Date = as.character(Date), # CHANGE DATE INTO A CHARACTER BUT HERE IT IS ALREADY A CHARACTER
 CondData<-read_csv(here("Week_05","Data","CondData.csv"))
-CondData<-CondData %>%
-  mutate(Date = as.character(Date), # change a column into a character
-         Date = mdy_hm(Date)) %>% # change date format to mdy_hm
-  round_date(datetimes, "10 seconds") # round to nearest 10 seconds) 
+CondData_iso<-CondData %>%
+  #Date = as.character(Date), # change a column into a character
+  mutate(date = mdy_hm(date) %>% # change date format to mdy_hm
+  date = round_date(date, "10 seconds")) # round to nearest 10 seconds) 
 
-View(CondData)
-glimpse(CondData)
-
-###  JOIN TWO DATA SETS TOGETHER ### 
-ConjoinedData<-inner_join(CondData, DepthData)
-  
+Cond_depth_data <- inner_join(CondData_iso, DepthData)
+View(Cond_depth_data)
 
 
 
-
-#####
-  
-  hour(Date) # tells me the hours
-  minute(Date) # tells me the minutes
-    summarise(Param_average =mean(Salinity,na.rm = TRUE), # get the mean
-              Param_average =mean(Temperature,na.rm = TRUE),
-              Param_average =mean(Date,na.rm =TRUE),
-              Param_average =mean(Depth,na.rm =TRUE),
-  
-View(NewData)
-   
+          
 
   
 
+## plotting time with averaged data ##
 
 
 
