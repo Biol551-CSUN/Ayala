aa0ae1f78f58d8b231b7964d612999c33c5b1bc6
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-25 19:12:33 -0800

    updated script for hw assignment 2023-02-23

--- Week_05/Script/HomeworkLabAssignmentDateandTimes(2023-02-23).R
+++ Week_05/Script/HomeworkLabAssignmentDateandTimes(2023-02-23).R
@@ -7,37 +7,54 @@ library(tidyverse)
 library(here)
 library(lubridate)
 library(devtools)
-library(ggridges)
+library(ggplot2)
+library(beyonce)
 
 install_github("Gibbsdavidl/CatterPlots") # install the data
 
 #### Loading Data ###
 
-# Depth data
 DepthData<-read_csv(here("Week_05","Data","DepthData.csv")) 
-DepthData<-DepthData %>%
-  mutate(Date = mdy_hm(Date)) # change date format to mdy_hm
+CondData<-read_csv(here("Week_05","Data","CondData.csv"))
+
+Condmutated<-CondData %>%
+  mutate(date= mdy_hms(date),# change date format to mdy_hm
+         date= round_date(date, "10 seconds")) #round to nearest 10 seconds
+
+### VIEW THE DATA SETS ###
 
 View(DepthData)
-glimpse(DepthData)
+View(CondData)
+View(Condmutated)
 
-# Conductivity data from each site
-#mutate(Date = as.character(Date), # CHANGE DATE INTO A CHARACTER BUT HERE IT IS ALREADY A CHARACTER
-CondData<-read_csv(here("Week_05","Data","CondData.csv"))
-CondData_iso<-CondData %>%
-  #Date = as.character(Date), # change a column into a character
-  mutate(Date = mdy_hm(Date) %>% # change date format to mdy_hm
-           Date = round_date(Date, "10 seconds")) # round to nearest 10 seconds) 
+### JOIN TWO DATA SETS TOGETHER ###
 
-Cond_depth_data <- inner_join(CondData_iso, DepthData)
-View(Cond_depth_data)
+inner_join(Condmutated, DepthData) %>% # we are joining exact datasets from each one
+  mutate(date= round_date(date, "1 min")) %>%  # round the time to nearest minutes
+  group_by(date)%>% # grouping by date :)
+  summarize(depthaverage=mean(Depth, na.rm= TRUE), #average of depth
+            tempaverage= mean(Temperature, na.rm= TRUE), # average of temp
+            salinityaverage= mean(Salinity, na.rm= TRUE))%>% # average of salinity
+  
+  #### TIME TO PLOT ####
+
+  ggplot(aes(x= date, # wanna graph the date on x
+             y= salinityaverage))+ # wanna graph the average of salinity on y
+  geom_line(color= "darkblue")+ 
+  labs(x= "Time",
+       y= "Average Salinity",
+       title= "Average Salinity at Various Time Points") + # the plot title
+  scale_color_manual(values = beyonce_palette(3)) +
+  theme_linedraw() + 
+  theme(axis.title = element_text(size = 20,
+                                  color = "darkgray"),
+        panel.background = element_rect(fill = "linen"))
+       
+  
+ggsave(here("Week_05","Output","newplot.png"))
 
 
 
 
-
-
-
-## plotting time with averaged data ##
 
 
